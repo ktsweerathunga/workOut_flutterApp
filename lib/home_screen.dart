a9cdb955d60993010704330f2e9858e0b4a05e94
Sandinu Weerathunga <sandinuweerathunga3@gmail.com>
2025-09-18 01:21:58 +0530

    addin ui buttons

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,19 +21,44 @@ class _HomeScreenState extends State<HomeScreen> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Row(
-                  children: [
-                    Text("data"),
-                    Text("data"),
-                  ],
-                )
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text("Hey,", style: GoogleFonts.bebasNeue(
+                        fontSize: 40,
+                        color: Colors.white,
+                        letterSpacing: 1.8
+                      ),),
+                      Text("Big guy", style: GoogleFonts.bebasNeue(
+                        fontSize: 40,
+                        color: const Color.fromARGB(255, 90, 188, 74),
+                        letterSpacing: 1.8
+                      ),),
+                    ],
+                  ),
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(color: Color.fromARGB(255, 90, 188, 74), width: 3),
+                      image: DecorationImage(image: AssetImage("assets/images/emely.jpg"), fit: BoxFit.cover)
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                height: 300,
+                child: Icon(Icons.play_circle_sharp , size: 80, color: Colors.white54,),
+              )
+            ],
+          ),
         ),
       ) 
     );
