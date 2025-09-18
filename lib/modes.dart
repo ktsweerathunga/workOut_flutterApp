d6a2c899c9e436bac1dfbf4d5961e122eca05d64
Sandinu Weerathunga <sandinuweerathunga3@gmail.com>
2025-09-18 18:15:39 +0530

    refactor: reorganize Exercise class structure for clarity

--- lib/modes.dart
+++ lib/modes.dart
@@ -4,3 +4,23 @@ class Category {
 
   Category({required this.name, required this.imageUrl});
 }
+
+class Exercise {
+  final String name;
+  final String description;
+  final int sets;
+  final int reps;
+  final int duration; // in seconds
+  final List<String> muscleGroups;
+  final String imageUrl;
+
+  Exercise({
+    required this.name,
+    required this.description,
+    required this.sets,
+    required this.reps,
+    required this.duration,
+    required this.muscleGroups,
+    required this.imageUrl,
+  });
+}
